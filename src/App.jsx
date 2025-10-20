e442843e5198ebd30c1386ffd4ae2a98fa1e3f4c
Thang Van Khual <khuallp996@gmail.com>
2025-10-20 19:41:26 +0800

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NavBar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+ <main>
+  <NavBar/>
+ </main>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navlinks = [
+                    {lable : 'Store', link: '#'},
+                     {lable : 'Mac', link: '#'},
+                    {lable : 'iPhone', link: '#'}, 
+                    {lable : 'Watch', link: '#'},
+                    {lable : 'Vision', link: '#'},
+                       {lable : 'Airpods', link: '#'},
+                ];
