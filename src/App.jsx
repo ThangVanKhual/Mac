a5c24d10969cdc4af8e6b9d5408936f22cd05f47
Thang Van Khual <khuallp996@gmail.com>
2025-10-22 23:03:33 +0800

    Add Hero component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NavBar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 const App = () => {
   return (
  <main>
   <NavBar/>
+  <Hero/>
  </main>
   )
 }
